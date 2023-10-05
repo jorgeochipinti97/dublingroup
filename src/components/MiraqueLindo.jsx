20ba3e1479196b7945a77d812cdfc4f45bc3cf30
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2023-10-05 14:52:56 -0300

    2

--- src/components/MiraqueLindo.jsx
+++ src/components/MiraqueLindo.jsx
@@ -10,7 +10,7 @@ export const MiraqueLindo = () => {
     }, {
       xPercent: 0, opacity: 1, ease: Power1.easeIn, duration: 1.5
     })
-    gsap.fromTo('#miraquebueno', { xPercent: 1, opacity: 0.5, }, { xPercent: 0, opacity: 1, ease: Power1.easeIn, duration: 1.5 })
+    gsap.fromTo('#miraquebueno', { yPercent: 10, opacity: 0.5, }, { yPercent: 0, opacity: 1, ease: Power1.easeIn, duration: 1.5 })
     gsap.fromTo('.buttonIphones', { opacity: 0 }, { opacity: 1, ease: Power1.easeIn, delay: 1.5, duration: .5 })
 
   }, [])
@@ -18,19 +18,21 @@ export const MiraqueLindo = () => {
     <>
 
       <div className=" flex items-center justify-center pt-5 miraque" style={{ opacity: 0 }}>
-        <div className="flex justify-around md:justify-center items-center" >
-      <img src='/section2iphones.png' class='w-3/5 md:w-2/5 mx-0 md:1x-10' alt='foto'/>
+        <div className="flex justify-around  items-center  " >
+          <img src='/section2iphones.png' class='w-3/5 md:w-2/5 mx-0 md:1x-10' alt='foto' />
           <p className="md:text-6xl text-2xl text-center md:text-start  " >Mirá que <br className='sm:hidden' /> lindo</p>
         </div >
       </div >
-      <div className="w-full flex justify-center mt-10 " >
-        <div className=" flex items-center justify-center " style={{ opacity: 0,maxWidth:'100vw' }} id='miraquebueno' >
-          <div className="flex w-full justify-center items-center w-3/5 md:w-2/5 ">
-            <p className="md:text-6xl text-2xl  text-center md:text-end ">Mirá que  <br className='sm:hidden' />bueno</p>
-          </div>
-          <img src="/iphones.svg" alt="iphones" className="md:mx-10 mx-2  w-3/5" />
+
+      <div className=" flex items-center mt-10 justify-center" style={{ opacity: 0, }} id='miraquebueno' >
+
+        <p className="md:text-6xl text-2xl  text-center md:text-end ">Mirá que  <br className='md:hidden' />bueno</p>
+
+        <div className='flex justify-center md:mx-0 mx-4 w-3/5 md:w-2/5 ' >
+          <img src="/iphones.svg" alt="iphones" />
         </div>
       </div>
+
 
       <a href="https://api.whatsapp.com/send/?phone=5491136740350&text&type=phone_number&app_absent=0">
         <div className="w-full flex justify-center mt-20 buttonIphones flex-wrap " style={{ opacity: 0, }}  >
